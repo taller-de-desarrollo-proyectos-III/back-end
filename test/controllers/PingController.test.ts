9bba498e0ddfc9b5c816fd0b8c59a3c882e35090
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-10-07 16:58:51 -0300

    Remove unnecesary space

--- test/controllers/PingController.test.ts
+++ test/controllers/PingController.test.ts
@@ -2,7 +2,6 @@ import { testClient } from "./TestClient";
 import { PingRoutes } from "../../src/routes/PingRoutes";
 
 describe("PingController", () => {
-
   it("returns a message", async () => {
     const request = await testClient.get(PingRoutes.path);
     expect(request.text).toEqual("pong");
