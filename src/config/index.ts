1e4ec2dccb9fd3bccc921b8b41b5a64fee2d1f78
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-10-14 19:46:29 -0300

    Add cos policy

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,3 @@
 export * from "./database";
 export * from "./Environment";
+export * from "./Cors";
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,6 +1,8 @@
 import bodyParser from "body-parser";
+import { cors } from "./Cors";
 
 export const middlewares = [
   bodyParser.json(),
-  bodyParser.urlencoded({ extended: true })
+  bodyParser.urlencoded({ extended: true }),
+  cors()
 ];
