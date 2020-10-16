c768a4c42eb7a7903bfcc367f61ee8b3348c6776
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-10-16 05:07:37 -0300

    Use mockImplementation instead if mockImplementationOnce

--- test/config/Environment.test.ts
+++ test/config/Environment.test.ts
@@ -2,40 +2,40 @@ import { Environment } from "../../src/config";
 
 describe("Environment", () => {
   it("checks for the presence of all environment variables", async () => {
-    jest.spyOn(Environment, "isLocal").mockImplementationOnce(() => false);
-    jest.spyOn(Environment, "NODE_ENV").mockImplementationOnce(() => Environment.TEST);
-    jest.spyOn(Environment.database, "url").mockImplementationOnce(() => "someUrl");
+    jest.spyOn(Environment, "isLocal").mockImplementation(() => false);
+    jest.spyOn(Environment, "NODE_ENV").mockImplementation(() => Environment.TEST);
+    jest.spyOn(Environment.database, "url").mockImplementation(() => "someUrl");
     expect(() => Environment.validate()).not.toThrow();
   });
 
   it("throws an error if the database url is not defined", async () => {
-    jest.spyOn(Environment, "isLocal").mockImplementationOnce(() => false);
-    jest.spyOn(Environment, "NODE_ENV").mockImplementationOnce(() => Environment.TEST);
-    jest.spyOn(Environment.database, "url").mockImplementationOnce(() => undefined as any);
+    jest.spyOn(Environment, "isLocal").mockImplementation(() => false);
+    jest.spyOn(Environment, "NODE_ENV").mockImplementation(() => Environment.TEST);
+    jest.spyOn(Environment.database, "url").mockImplementation(() => undefined as any);
     expect(() => Environment.validate()).toThrow("Missing environment variable: DATABASE_URL");
   });
 
   it("throws an error if the NODE_ENV is not defined", async () => {
-    jest.spyOn(Environment, "isLocal").mockImplementationOnce(() => false);
-    jest.spyOn(Environment, "NODE_ENV").mockImplementationOnce(() => undefined as any);
-    jest.spyOn(Environment.database, "url").mockImplementationOnce(() => "someUrl");
+    jest.spyOn(Environment, "isLocal").mockImplementation(() => false);
+    jest.spyOn(Environment, "NODE_ENV").mockImplementation(() => undefined as any);
+    jest.spyOn(Environment.database, "url").mockImplementation(() => "someUrl");
     expect(() => Environment.validate()).toThrow("Missing environment variable: NODE_ENV");
   });
 
   it("throws an error if the NODE_ENV has an unknown value", async () => {
-    jest.spyOn(Environment, "isLocal").mockImplementationOnce(() => false);
-    jest.spyOn(Environment, "NODE_ENV").mockImplementationOnce(() => "unknownValue" as any);
-    jest.spyOn(Environment.database, "url").mockImplementationOnce(() => "someUrl");
+    jest.spyOn(Environment, "isLocal").mockImplementation(() => false);
+    jest.spyOn(Environment, "NODE_ENV").mockImplementation(() => "unknownValue" as any);
+    jest.spyOn(Environment.database, "url").mockImplementation(() => "someUrl");
     expect(() => Environment.validate()).toThrow("Missing environment variable: NODE_ENV");
   });
 
   it("skips validation if NODE_ENV is development", async () => {
-    jest.spyOn(Environment, "NODE_ENV").mockImplementationOnce(() => Environment.DEVELOPMENT);
+    jest.spyOn(Environment, "NODE_ENV").mockImplementation(() => Environment.DEVELOPMENT);
     expect(() => Environment.validate()).not.toThrow();
   });
 
   it("skips validation if NODE_ENV is test", async () => {
-    jest.spyOn(Environment, "NODE_ENV").mockImplementationOnce(() => Environment.TEST);
+    jest.spyOn(Environment, "NODE_ENV").mockImplementation(() => Environment.TEST);
     expect(() => Environment.validate()).not.toThrow();
   });
 
