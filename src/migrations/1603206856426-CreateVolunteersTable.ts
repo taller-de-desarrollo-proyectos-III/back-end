366406a6081f26b4cf2c1adfc0faa879af77fbdf
Lautaro <lautarodelosheros@gmail.com>
2020-10-20 14:39:35 -0300

    Adding migration for Volunteers table

---
+++ src/migrations/1603206856426-CreateVolunteersTable.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateVolunteersTable1603206856426 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    return queryRunner.createTable(new Table({
+      name: "Volunteers",
+      columns: [
+        {
+          name: "uuid",
+          type: "uuid",
+          isPrimary: true,
+          isNullable: false
+        },
+        {
+          name: "dni",
+          type: "varchar",
+          isNullable: false
+        },
+        {
+          name: "name",
+          type: "varchar",
+          isNullable: false
+        },
+        {
+          name: "surname",
+          type: "varchar",
+          isNullable: false
+        }
+      ]
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return queryRunner.dropTable("Volunteers");
+  }
+}
