727f501845846719be4f4b39d084ef1d791813d0
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-22 18:55:52 -0300

    Add frontend url for production cors policy

--- src/config/Cors.ts
+++ src/config/Cors.ts
@@ -2,7 +2,7 @@ import { Environment } from "./Environment";
 
 export const CorsConfig = {
   production: {
-    origin: "https://tdp3-frontend.herokuapp.com",
+    origin: "https://nahual-portal-voluntaries.herokuapp.com",
     credentials: true,
     optionsSuccessStatus: 200
   },
@@ -12,17 +12,17 @@ export const CorsConfig = {
     optionsSuccessStatus: 200
   },
   development: {
-    origin: "http://localhost:3001",
+    origin: ["http://localhost:3000", "http://localhost:3001"],
     credentials: true,
     optionsSuccessStatus: 200
   },
   test: {
-    origin: "http://localhost:3001",
+    origin: ["http://localhost:3000", "http://localhost:3001"],
     credentials: true,
     optionsSuccessStatus: 200
   },
   travis: {
-    origin: "http://localhost:3001",
+    origin: ["http://localhost:3000", "http://localhost:3001"],
     credentials: true,
     optionsSuccessStatus: 200
   }
