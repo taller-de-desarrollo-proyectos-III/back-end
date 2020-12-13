f34238003c1ab1087654af75d2ce3cc18c97f2a0
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-12 22:18:24 -0300

    Add stateUuid in the volunteer script

--- scripts/createVolunteers.sh
+++ scripts/createVolunteers.sh
@@ -3,9 +3,15 @@ commissions=$(curl --request GET \
   --header 'content-type: application/json' \
   --cookie _session_id=2777ff79d5e1e5d8bb389b2e45230a66)
 
+states=$(curl --request GET \
+  --url http://localhost:5000/states \
+  --header 'content-type: application/json' \
+  --cookie _session_id=2777ff79d5e1e5d8bb389b2e45230a66)
+
 # Maybe you need to install jq: sudo apt-get install jq
 
 commissionUuids=($(echo "$commissions" | jq -r '.[].uuid' /dev/stdin))
+stateUuids=($(echo "$states" | jq -r '.[].uuid' /dev/stdin))
 
 names=("Juan" "Pedro" "Pablo" "Jorge" "Juana" "Marta" "Celia" "Sofía" "Romina" "Candela" "Horacio" "Tadeo" "Camilo")
 surnames=("García" "Martinez" "Pérez" "Rodriguez" "Gutiérrez" "Solari")
@@ -13,6 +19,7 @@ surnames=("García" "Martinez" "Pérez" "Rodriguez" "Gutiérrez" "Solari")
 for commissionUuid in "${commissionUuids[@]}"
 do
 
+  stateUuid=${stateUuids[$RANDOM % ${#stateUuids[@]} ]}
   name=${names[$RANDOM % ${#names[@]} ]}
   surname=${surnames[$RANDOM % ${#surnames[@]} ]}
   dni=$((10000000 + RANDOM))
@@ -32,7 +39,8 @@ do
       "dni": "'"$dni"'",
       "email": "'"$email"'",
       "phoneNumber": "'"$phoneNumber"'",
-      "notes": "Notes example"
+      "notes": "Notes example",
+      "stateUuid": "'"$stateUuid"'"
   }'
 
 done
